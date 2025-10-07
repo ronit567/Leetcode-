3578b1dcfc869255fcf7bb4f1de7c21276de5458
ronit567 <ronlongia@gmail.com>
2025-10-07 11:54:48 -0400

    Roman Numbers

---
+++ romantoenglish.py
@@ -0,0 +1,18 @@
+def int_to_roman(num: int) -> str:
+    """Convert an integer to a Roman numeral."""
+
+    roman_map = [
+        ("M", 1000),
+        ("CM", 900),
+        ("D", 500),
+        ("CD", 400),
+        ("C", 100),
+        ("XC", 90),
+        ("L", 50),
+        ("XL", 40),
+        ("X", 10),
+        ("IX", 9),
+        ("V", 5),
+        ("IV", 4),
+        ("I", 1)
+    ]
