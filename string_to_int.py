9c83c3c4a66395bb785a8310dd3c1dbf6b572e06
ronit567 <ronlongia@gmail.com>
2025-10-08 16:23:46 -0400

    Leading Zeros

--- string_to_int.py
+++ string_to_int.py
@@ -18,17 +18,33 @@ def myAtoi(s: str) -> int:
     # 1) skip leading whitespace
     while i < n and s[i] == ' ':
         i += 1
-
-    # if only spaces
     if i == n:
         return 0
 
     # 2) signedness
     sign = 1
-    if s[i] == '+' or s[i] == '-':
-        if s[i] == '-':
-            sign = -1
+    if s[i] in ('+', '-'):
+        sign = -1 if s[i] == '-' else 1
         i += 1
 
-    # parsing to be added
-    return 0
+    # 3) read digits (ignore leading zeros explicitly)
+    #    stop at first non-digit
+    num_started = False
+    value = 0
+
+    # skip leading zeros in the numeric portion
+    while i < n and s[i] == '0':
+        num_started = True
+        i += 1
+
+    # read remaining digits
+    while i < n and s[i].isdigit():
+        num_started = True
+        digit = ord(s[i]) - ord('0')
+        value = value * 10 + digit
+        i += 1
+
+    if not num_started:
+        return 0
+
+    return sign * value
