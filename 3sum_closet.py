f799f152d5c1b5787fe819301cf625d903200c78
ronit567 <ronlongia@gmail.com>
2025-10-11 17:02:07 -0400

    Fixed Names

--- 3sum_closet.py
+++ 3sum_closet.py
@@ -6,16 +6,16 @@ def threeSumClosest(nums, target):
         left, right = i + 1, len(nums) - 1
         
         while left < right:
-            current_sum = nums[i] + nums[left] + nums[right]
+            cur_sum = nums[i] + nums[left] + nums[right]
             
-            if abs(target - current_sum) < abs(target - closest_sum):
-                closest_sum = current_sum
+            if abs(target - cur_sum) < abs(target - closest_sum):
+                closest_sum = cur_sum
             
-            if current_sum < target:
+            if cur_sum < target:
                 left += 1
-            elif current_sum > target:
+            elif cur_sum > target:
                 right -= 1
             else:
-                return current_sum
+                return cur_sum
     
     return closest_sum
