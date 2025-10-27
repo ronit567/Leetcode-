f88432124c8a2fb1dc52277cde0fa0e5baa683fe
ronit567 <ronlongia@gmail.com>
2025-10-27 03:15:11 -0400

    Initialize Sudoku validation function with input parsing

---
+++ sudoku.py
@@ -0,0 +1,7 @@
+def isValidSudoku(board):
+    """
+    Check if a given 9x9 Sudoku board is valid.
+    Only filled cells are validated according to Sudoku rules.
+    """
+    # TODO: Implement row, column, and sub-box validation logic
+    pass
